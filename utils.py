9e89b00802a7081b4ab7a81ab643c7685d9626b7
Edaad <edaad.azman57@gmail.com>
2024-09-13 10:39:28 -0400

    utils implementation

--- utils.py
+++ utils.py
@@ -2,40 +2,55 @@
 
 import numpy as np
 
+
 def dot_product(v1, v2):
-    '''
+    """
     v1 and v2 are vectors of same shape.
     return the scalar dor product of the two vectors.
     # Hint: use `np.dot`.
-    '''
+    """
     ### YOUR CODE HERE
-    
+    return np.dot(v1, v2)
+
+
 def cosine_similarity(v1, v2):
-    '''
+    """
     v1 and v2 are vectors of same shape.
     Return the cosine similarity between the two vectors.
-    
-    # Note: The cosine similarity is a commonly used similarity 
-    metric between two vectors. It is the cosine of the angle between 
+
+    # Note: The cosine similarity is a commonly used similarity
+    metric between two vectors. It is the cosine of the angle between
     two vectors, and always between -1 and 1.
-    
-    # The formula for cosine similarity is: 
+
+    # The formula for cosine similarity is:
     # (v1 dot v2) / (||v1|| * ||v2||)
-    
+
     # ||v1|| is the 2-norm (Euclidean length) of the vector v1.
-    
+
     # Hint: Use `dot_product` and `np.linalg.norm`.
-    '''
+    """
     ### YOUR CODE HERE
-    
+    numerator = dot_product(v1, v2)
+    denominator = np.linalg.norm(v1) * np.linalg.norm(v2)
+    return numerator / denominator
+
+
 def nearest_neighbor(target_vector, vectors):
-    '''
+    """
     target_vector is a vector of shape d.
     vectors is a matrix of shape N x d.
-    return the row index of the vector in vectors that is closest to 
+    return the row index of the vector in vectors that is closest to
     target_vector in terms of cosine similarity.
-    
+
     # Hint: You should use the cosine_similarity function that you already wrote.
     # Hint: For this lab, you can just use a for loop to iterate through vectors.
-    '''
+    """
     ### YOUR CODE HERE
+    max_similarity = -np.inf
+    nearest_index = -1
+    for idx, vector in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vector)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            nearest_index = idx
+    return nearest_index
